cf061b2e71799e47570562b8246920f6f7b28aae
Susan Rehl <susanrehl@gmail.com>
2016-07-08 11:55:20 -0500

    Query basics working and displaying to DOM

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,19 +2,72 @@ console.log('script.js is sourced');
 
 var myApp=angular.module('myApp', []);
 
-var disinfectantList=[]; // creates array of disinfectant results
+// app.factory('myService', function() {
+//  var savedData = {};
+//  function set(data) {
+//    savedData = data; }
+//  function get() {
+//   return savedData;
+//  }
+//  return {
+//   set: set,
+//   get: get
+// };
+// });
+// myService.set(yourSharedData);   //put into questionnaire controller
+// $scope.desiredLocation = myService.get(); // put into results controller
+// Remember to inject myService in the controllers by passing it as a parameter.
 
-myApp.controller('productController', ['$scope', '$http', function($scope, $http) {
-  $scope.getProducts = function() {
-      console.log("clicked submit button");
+myApp.factory('DataService', function() {
+    var choices = {};
+    function set(data) {
+      choices=data;
+    } // end function set data
+    function get() {
+      return choices;
+    } // end function get
+    return {
+      set: set,
+      get: get
+    }; // end return
+  }); // end factory
+
+myApp.controller('productController', ['$scope', '$http', 'DataService', function($scope, $http, DataService) {
+
+  DataService.set(yourSharedData);
+
+  $scope.disinfectantList=[]; // creates array of disinfectant results
+
+  $scope.selectProducts = function() {
+      event.preventDefault();
+      var objectToSend ={  // package inputs into object to send
+        areas: $scope.places,  // reference these in questionnaire.html
+        surfaces: $scope.surfaces,
+        pathogens: $scope.pathogens,
+        format: $scope.format
+        }; // end object
+      $http({  // sends object via POST
+        method: 'POST',
+        url: '/select',
+        data: objectToSend
+      }); // end post call
+    var path="/results";  // sends user to results page on click
+    window.location.href=path;
+  }; // end selectProduct function
+
+    $scope.getProducts = function() {
       $http({   // gets recordset via GET
         method: 'GET',
         url: '/list',
-      }).then( function( response ){  // success call - runs function with response parameter
-        // console.log(response);
-        $scope.disinfectantList = response.data;  // pulls the data from app.js and sets to disinfectantList
-      }, function myError( response ){
-        console.log( response.statusText );
-      }); // end error function
+      }).then( function(response){  // success call - runs function with response parameter
+      // console.log(response.data);
+      $scope.disinfectantList = response.data;  // pulls the data from app.js and sets to disinfectantList
+      // console.log("log from select function in script:");
+      // console.log($scope.disinfectantList);
+    }, function myError(response){
+      console.log(response.statusText);
+      }// end error function
+    ); // end then response
   }; // end getProducts function
-}]);
+
+}]); // end myApp controller
--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var urlencodedParser=bodyParser.urlencoded( {extended: false} );
 var pg=require('pg');  // sets up postgres database
 var connectionString='postgres://localhost:5432/disinfectants';
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {  //makes home page available
   res.sendFile(path.resolve('views/index.html'));
@@ -20,29 +20,41 @@ app.get( '/results', function( req, res ){  // makes results.html available
   res.sendFile( path.resolve( 'views/results.html' ) );
   });
 
-app.get('/list', function(req, res) { // displaying to do list - uses GET
-    console.log("in app.get get disinfectants list");
-    var results = [];  // array to hold tasks
+var selectedProducts;
+
+app.post('/select', urlencodedParser, function(req, res) { // displaying to do list - uses GET
+    console.log("in app.post select disinfectants");
+    selectedProducts = [];  // array to hold tasks
     pg.connect(connectionString, function(err, client, done) {  // connecting to todolist database
       if (err) {     // check for errors
-      consle.log(err);
+      console.log(err);
     } else {
-        var products=client.query('SELECT * FROM products ORDER BY productname;');  // getting products from products table
-        console.log('query '+ products);
+        var products=client.query("SELECT * FROM products WHERE areas= '" + req.body.areas + "'");  // getting products from products table
         var rows = 0;
         products.on('row', function(row) {  // pushing to array
-          results.push(row);
+          selectedProducts.push(row);
         });  // end query push
         products.on('end', function() {  // sending to scripts
-          return res.json(results);
+          console.log("Here are the selected products from app.post in app");
+          console.log(selectedProducts);
+          return res.json(selectedProducts);
         }); // end products.on function
       done(); // signals done
     } // end else
   }); // end pg connect function
-}); // end app.get /getlist function
+}); // end app.post /list function
+
+app.get( '/list', function( req, res ){  // makes returned list of products available to results.html
+      return res.json(selectedProducts);
+  }); // end /list GET function
 
 app.listen(8080, 'localhost', function(req, res) {
   console.log('listening on 8080');
-});
+});  // end app.listen
 
+app.set("port", (process.env.PORT || 8080));
+
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: ", app.get("port"));
+});
 app.use(express.static('public'));
